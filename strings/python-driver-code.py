180a74bd55c45e316a235ae477cc4d465073e9e2
DharaniPenta <105910327+DharaniPenta@users.noreply.github.com>
2022-05-20 13:17:03 +0530

    Update python-driver-code.py

--- strings/python-driver-code.py
+++ strings/python-driver-code.py
@@ -3,7 +3,21 @@
 def solve(s: str) -> chr:
   # Your code goes here
   # s is the given input string
-  return 
+  l=s.split()
+  s1=l[0]
+  s2=l[1]
+  li11=[int(i) for i in s1]
+  li22=[int(i) for i in s2]
+  li11.sort()
+  li222.sort()
+  list1=[str(i) for i in li11]
+  list2=[str(i) for i in li22]
+  c1=''.join(list1)
+  c2=''.join(list2)
+  if c1==c2:
+    return 'recycled pair'
+  else:
+    return 'not recycled pair'
 
 # The following snippet reads the input in the required format. 
 # Just complete the solve function above. 
